20aa58436dea005907908c34c00e198dd8b11649
haya14busa <haya14busa@gmail.com>
2025-10-07 12:55:30 +0900

    chore(lint): remove unused function to satisfy oxlint

--- src/graphql/pr-files-queries.ts
+++ src/graphql/pr-files-queries.ts
@@ -8,12 +8,6 @@ export type FilterByChangedFilesParams = {
 	graphqlFn: GraphQLFn;
 };
 
-function chunk<T>(arr: T[], size: number): T[][] {
-	const out: T[][] = [];
-	for (let i = 0; i < arr.length; i += size) out.push(arr.slice(i, i + size));
-	return out;
-}
-
 function buildFilesBatchQuery(
 	prNumbers: number[],
 	varPrefix = "after",
